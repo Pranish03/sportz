9daa24fc8b09a7cbdba1671e8e966e0136f4b9c6
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-08-31 21:54:01 +0545

    feat: implement arcjet security

--- src/ws/server.js
+++ src/ws/server.js
@@ -1,5 +1,5 @@
 import WebSocket, { WebSocketServer } from "ws";
-
+import { wsArcjet } from "../arcjet.js";
 
 function sendJson(socket, payload) {
     if (socket.readyState !== WebSocket.OPEN) return;
@@ -18,11 +18,49 @@ function broadcast(wss, payload) {
 export function attactWebSocketServer(server) {
     const wss = new WebSocketServer({ server, path: "/ws", maxPayload: 1024 * 1024 });
 
-    wss.on('connection', (socket) => {
+    wss.on('connection', async (socket, req) => {
+
+        if (wsArcjet) {
+            try {
+                const decision = await wsArcjet.protect(req);
+
+                if (decision.isDenied()) {
+                    const code = decision.reason.isRateLimit ? 1013 : 1008;
+                    const reason = decision.reason.isRateLimit ? 'Too many requests' : 'Access denied';
+
+                    return socket.close(code, reason);
+                }
+            } catch (error) {
+                console.error("WS connection error:", error);
+                return socket.close(1011, "Service unavailable");
+            }
+        }
+
+        socket.isAlive = true;
+
+        socket.on('pong', () => {
+            socket.isAlive = true;
+        })
+
+        socket.on('close', () => {
+            socket.isAlive = false;
+        })
+
         sendJson(socket, { type: 'welcome' })
 
         socket.on('error', console.error)
     })
+
+    const interval = setInterval(() => {
+        wss.clients.forEach((ws) => {
+            if (!ws.isAlive) return ws.terminate();
+
+            ws.isAlive = false;
+            ws.ping();
+        })
+    }, 3000)
+
+    wss.on('close', () => clearInterval(interval));
 
     function broadcastMatchCreated(match) {
         broadcast(wss, { type: 'match_created', data: match })
